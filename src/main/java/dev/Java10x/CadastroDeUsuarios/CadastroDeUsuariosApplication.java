3203d41ad5e675d9b98242eed13a74d3dcdf218a
Roger Viliano <rogrogvil@gmail.com>
2026-02-17 20:15:20 -0300

    first commit

---
+++ src/main/java/dev/Java10x/CadastroDeUsuarios/CadastroDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package dev.Java10x.CadastroDeUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeUsuariosApplication.class, args);
+	}
+
+}
