e2350c38131371762bc8f2c1d0e1d1aa998c5834
Roger Viliano <rogrogvil@gmail.com>
2026-02-17 21:01:38 -0300

    add first controller

---
+++ src/main/java/dev/Java10x/CadastroDeUsuarios/Controller.java
@@ -0,0 +1,14 @@
+package dev.Java10x.CadastroDeUsuarios;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+   @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Bem vindo - Essa é a primeira mensagem dessa Rota";
+    }
+}
