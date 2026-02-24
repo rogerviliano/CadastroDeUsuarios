0c6156fed877c4e297c122a235698cb35bcbcfc3
Roger Viliano <rogrogvil@gmail.com>
2026-02-24 18:38:55 -0300

    feat: adiciona a interface AtividadesRepository extendendo JpaRepository

---
+++ src/main/java/dev/Java10x/CadastroDeUsuarios/Atividades/AtividadesRepository.java
@@ -0,0 +1,7 @@
+package dev.Java10x.CadastroDeUsuarios.Atividades;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtividadesRepository extends JpaRepository<AtividadeModel,Long> {
+
+}
