47a5ffbe8c90901f26d314a1099d1362982a2ba9
Roger Viliano <rogrogvil@gmail.com>
2026-02-28 16:20:11 -0300

    feat: adiciona flyway como dependencia para versionar banco de dados e adiciona coluna nova a tabela tb_cadastro_usuarios

--- src/main/java/dev/Java10x/CadastroDeUsuarios/Users/UserModel.java
+++ src/main/java/dev/Java10x/CadastroDeUsuarios/Users/UserModel.java
@@ -21,6 +21,8 @@ public class UserModel {
     @Column(unique = true) //indica que nao pode haver dois registros iguais no BD para esta coluna
     private String email;
     private int idade;
+    private String cargo;
+    private int nivel;
     //Indica que N users podem ter Uma Atividade
     @ManyToOne
     @JoinColumn(name="atividade_id")//FK chave estrangeira do BD
