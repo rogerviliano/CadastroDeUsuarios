e21f2610c553c2fa94d3420bf7d2eeccd5f11b49
Roger Viliano <rogrogvil@gmail.com>
2026-02-28 17:20:45 -0300

    feat: adiciona os endpoints no controller do usuario - CRUD basico

--- src/main/java/dev/Java10x/CadastroDeUsuarios/Users/UserController.java
+++ src/main/java/dev/Java10x/CadastroDeUsuarios/Users/UserController.java
@@ -1,15 +1,46 @@
 package dev.Java10x.CadastroDeUsuarios.Users;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
-public class UserController {
+public class UserController { // A camada Controller detem os endpoints da API - CRUD basica)
+    //End point inicial - teste
     @GetMapping("/boasvindasuser")
     public String boasVindas(){
        return "Bem vindo - Essa é a primeira mensagem dessa Rota";
     }
+
+    //Metodos CRUD - CREATE, READ, UPDATE and DELETE - BASIC API
+
+    //Adicionar Usuario (CREATE)
+    @PostMapping("/criar")
+    public String criarUsuario(){
+        return "Ninja criado com sucesso";
+    }
+    //Mostrar ou pesquisar Usuario por _id (Read)
+    @GetMapping("/todosPorID")
+    public String mostrarTodosUsuarios() {
+        return "Mostrando ninja por ID";
+    }
+    //Mostrar todos os usuarios (Read)
+    @GetMapping("/todos")
+    public String mostrarTodosUsuarios(){
+        return "Mostrando todos os ninjas";
+    }
+    //Alterar dados de um usuario (UPDATE)
+    @PutMapping("AlterarID")
+    public String alterarPorId() {
+        return "Usuario alterado por Id com sucesso";
+    }
+
+    //Deletar usuário (Delete)
+    @DeleteMapping("/deletarID")
+    public String deletarUsuarioPorId(){
+        return "Usuario deletado com sucesso";
+    }
+
+
+
 
 }
